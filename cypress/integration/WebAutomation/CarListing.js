cc719f36c8d24422c9249a630e0384a531d7db03
Olabanj <olabanjolukman5@gmail.com>
2023-01-16 02:39:39 +0100

    fixture file add

--- cypress/integration/WebAutomation/CarListing.js
+++ cypress/integration/WebAutomation/CarListing.js
@@ -2,11 +2,16 @@
 import CarPage from "../POM/CarPage";
 const carPage = new CarPage()
 describe("Create a carlisting",function(){
+    before(function(){
+        cy.visit('/')
+ 
+    cy.fixture("CarData").then(function(data) {
+        this.data=data
+    })
+})
 it("Create carListing",function(){
-    //Login
-cy.visit('/')
-carPage.enterEmail().type("olabanjotboy@gmail.com");
-carPage.enterPassWord().type("Testing@123");
+carPage.enterEmail().type(this.data.Email);
+carPage.enterPassWord().type(this.data.Password);
 carPage.clickContBtn().click();
 carPage.enterFirstField().type('6');
 carPage.enterSecondField().type('8');
